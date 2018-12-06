c6e9e369f3af2aa19f83a1d6e022f32752d490fb
kay <schmitteckertkay@freenet.de>
2018-12-06 17:57:27 +0100

    well defined structure

---
+++ src/main/DataLoader.py
@@ -0,0 +1,4 @@
+from utils.utility import get_data
+
+print(get_data('A1', 'train'))
+print(get_data('A2', 'test', 'n'))
---
+++ src/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='kvv-simulator',
+      version='0.0.1',
+      install_requires=['pandas',
+                        'networkx',
+                        'gym',
+                        'cfg_load',
+                        ]
+      )
