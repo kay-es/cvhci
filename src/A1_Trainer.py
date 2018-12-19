c691a9d331ec6d6118a02e5735afaf84796803da
kay <schmitteckertkay@freenet.de>
2018-12-19 12:33:16 +0100

    cv

--- src/A1_Trainer.py
+++ src/A1_Trainer.py
@@ -136,7 +136,7 @@ def train(e, train_loader, valid_loader):
 
             # statistics
             running_loss += loss.item()
-            print('F1: {}'.format(f1_score(target.detach().cpu().numpy(), outputs.cpu().numpy() > 0.3, average="samples")))
+            print('F1: {}'.format(f1_score(target.cpu().detach().numpy(), outputs.cpu().detach().numpy() > 0.3, average="samples")))
             #running_corrects += torch.sum(preds == target.data)
 
         epoch_loss = running_loss / len(loaders[phase])
